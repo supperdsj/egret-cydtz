e57e66454ef8806634e90eebfbd7c6e69b513c58
dongsj <232171773@qq.com>
2019-02-13 15:12:31 +0800

    读取关卡

--- src/game/LevelDataManager.ts
+++ src/game/LevelDataManager.ts
@@ -1,11 +1,30 @@
-// class LevelDataItem{
-//    public answer:string;
-//    public img:string;
-//    public word:string;
-//    public tip:string;
-//    public content:string;
-// }
+interface LevelDataItem {
+   answer: string;
+   img: string;
+   word: string;
+   tip: string;
+   content: string;
+}
 
 class LevelDataManager {
    private static shared: LevelDataManager;
+
+   public static get Instance() {
+      if (!LevelDataManager.shared) {
+         LevelDataManager.shared = new LevelDataManager();
+      }
+      return LevelDataManager.shared;
+   }
+
+   public items: Array<LevelDataItem> = [];
+   public totalLevels: number = 0;
+
+   constructor() {
+      this.items = RES.getRes('questions_json');
+      this.totalLevels = this.items.length;
+   }
+
+   public getLevelData(level: number): LevelDataItem {
+      return this.items[level];
+   }
 }
